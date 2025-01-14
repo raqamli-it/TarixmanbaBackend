0bb2e4a89037117d1b7ca4b37171108e81d30183
asadbek000002 <asadbektuygunov9@gmail.com>
2025-01-14 17:17:29 +0500

    admin panel period filter add filter and search

--- resources/admin.py
+++ resources/admin.py
@@ -4,20 +4,30 @@ from .models import Category, FilterCategories, PeriodFilter, Filters, Resource,
     Gallery, File, Audio, VirtualReality, Video, \
     Location, Contents, Attributes
 
+
+class PeriodFilterAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category')  # Admin panelda ko'rsatiladigan ustunlar
+    search_fields = ('title', 'category__name')  # Qidiruvni qo'llab-quvvatlaydigan maydonlar
+    list_filter = ('category',)  # Filtrlarni qo'llab-quvvatlaydigan maydonlar
+
+
+admin.site.register(PeriodFilter, PeriodFilterAdmin)
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
 
+
 @admin.register(FilterCategories)
 class FilterCategoriesAdmin(admin.ModelAdmin):
-    list_display = ('id', 'category', 'title')  
+    list_display = ('id', 'category', 'title')
 
-
-admin.site.register(PeriodFilter)
 
 @admin.register(Filters)
 class FiltersAdmin(admin.ModelAdmin):
-    list_display = ('id', 'filter_category', 'title')  
+    list_display = ('id', 'filter_category', 'title')
+
 
 admin.site.register(Province)
 
@@ -75,22 +85,17 @@ class LocationInline(admin.TabularInline):
     extra = 1
 
 
-
-
-
 @admin.register(Resource)
 class ResourceAdmin(admin.ModelAdmin):
-    list_display = ('id','title', 'statehood',)
+    list_display = ('id', 'title', 'statehood',)
     search_fields = ('title',)
     list_filter = ['category']
     inlines = [
-        AttributesInline, ContentsInline, GalleryInline, FileInline,\
-        AudioInline, VirtualRealityInline, VideoInline, LocationInline,]
+        AttributesInline, ContentsInline, GalleryInline, FileInline, \
+        AudioInline, VirtualRealityInline, VideoInline, LocationInline, ]
 
     class Media:
         js = (
-            "dropdown.js", 
-            "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js", 
-            )
-        
-
+            "dropdown.js",
+            "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js",
+        )
